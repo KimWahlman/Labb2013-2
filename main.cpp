78434b39450ec189c63a55ee724423eabb5d8d96
Kim Wahlman <kim.wahlman@gmail.com>
2013-10-14 19:08:42 +0200

    Update main.cpp
    struff

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-// Main
+#include "MyRel.h"
+#include <iostream>
+#include <string>
+
+int main()
+{
+	// Domänermängder
+	std::set<int> ints;
+
+	int arrays[] = { 1, 2, 3, 4, 5, 6 };
+	ints.insert(arrays, arrays+6);
+	
+	MyRel R(ints);
+	R.add(std::pair<int, int>(R.domain[0], R.domain[5]));
+
+	
+	return 0;
+}
