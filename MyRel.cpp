60ae07987c3d6a52fe91cc2459c9714666618c02
Kim Wahlman <kim.wahlman@gmail.com>
2013-10-14 19:53:38 +0200

    Update MyRel.cpp
    Added a lot of functions!

--- MyRel.cpp
+++ MyRel.cpp
@@ -30,7 +30,7 @@ void MyRel::add(std::pair<int,int> P)
 
 void MyRel::add(std::set<std::pair<int,int>> PSet)
 {
-	mPairs.insert(PSet.begin, PSet.end);
+	mPairs.insert(PSet.begin(), PSet.end());
 }
 
 bool MyRel::q(std::pair<int, int> P)
@@ -41,4 +41,44 @@ bool MyRel::q(std::pair<int, int> P)
 			return true;
 	}
 	return false;
+}
+
+std::set<std::pair<int, int>> MyRel::pairSet()
+{
+	return mPairs;
+}
+
+MyRel MyRel::refl_closure()
+{
+	MyRel refl(domain());
+	refl.add(pairSet());
+	for(std::set<std::pair<int, int>>::iterator i = refl.mPairs.begin(); i != refl.mPairs.end(); i++)
+	{
+		if((*i).first != (*i).second)
+		{
+			std::pair<int, int> x((*i).first, (*i).first), y((*i).second, (*i).second);
+			if(!q(x))
+				refl.mPairs.insert(x);
+
+			if(!q(y))
+				refl.mPairs.insert(y);
+		}
+	}
+	return refl;
+}
+
+MyRel MyRel::symm_closure()
+{
+	MyRel symm(domain());
+	symm.add(pairSet());
+	for(std::set<std::pair<int, int>>::iterator i = symm.mPairs.begin(); i != symm.mPairs.end(); i++)
+	{
+		if((*i).first != (*i).second)
+		{
+			std::pair<int, int> x((*i).second, (*i).first);
+			if(!q(x))
+				symm.mPairs.insert(x);
+		}
+	}
+	return symm; // Lovar Inge, ingen copy paste!
 }
