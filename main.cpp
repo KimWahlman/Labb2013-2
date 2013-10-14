dfc31d13611a8c0154a20d22aa37b310a5e6ebdd
Kim Wahlman <kim.wahlman@gmail.com>
2013-10-14 21:24:27 +0200

    Update main.cpp
    Testing new functions

--- main.cpp
+++ main.cpp
@@ -10,12 +10,17 @@ int main()
 	int arrays[] = { 1, 2, 3, 4, 5, 6 };
 	ints.insert(arrays, arrays+6);
 	
-	MyRel R(ints);
-	R.add( std::pair<int, int>( ( *R.domain( ).find( 1 ) ), ( *R.domain( ).find( 2 ) ) ) );
-	R.add( std::pair<int, int>( ( *R.domain( ).find( 3 ) ), ( *R.domain( ).find( 4 ) ) ) );
-	R.add( std::pair<int, int>( ( *R.domain( ).find( 5 ) ), ( *R.domain( ).find( 6 ) ) ) );
-	//MyRel T = R.refl_closure();
-	MyRel S = R.symm_closure();
-	S = S.refl_closure();
+	MyRel Bas(ints);
+	
+	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 1 ) ), ( *Bas.domain( ).find( 2 ) ) ) );
+	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 3 ) ), ( *Bas.domain( ).find( 4 ) ) ) );
+	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 5 ) ), ( *Bas.domain( ).find( 6 ) ) ) );
+	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 2 ) ), ( *Bas.domain( ).find( 3 ) ) ) );
+	
+	MyRel Refl = Bas.refl_closure();
+	MyRel Trans = Bas.trans_closure();
+	MyRel Append = Bas;
+		  Append.append(trans);
+	MyRel Comp = Bas.comp(trans);
 	return 0;
 }
