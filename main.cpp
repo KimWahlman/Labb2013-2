e3db06c190eb7903de0ac6edffd9b572433c2a5f
Kim Wahlman <kim.wahlman@gmail.com>
2013-10-16 12:32:32 +0200

    Update main.cpp
    Fixade output och lite annat

--- main.cpp
+++ main.cpp
@@ -16,11 +16,39 @@ int main()
 	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 3 ) ), ( *Bas.domain( ).find( 4 ) ) ) );
 	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 5 ) ), ( *Bas.domain( ).find( 6 ) ) ) );
 	Bas.add( std::pair<int, int>( ( *Bas.domain( ).find( 2 ) ), ( *Bas.domain( ).find( 3 ) ) ) );
-	
+
+	std::cout << "Grund settet\n";
+	Bas.printSet();
+	Bas.printPairs();
+
 	MyRel Refl = Bas.refl_closure();
+	std::cout << "\n\nReflektiva settet\n";
+	Refl.printSet();
+	Refl.printPairs();
+
+	MyRel  Symm = Bas.symm_closure();
+	std::cout << "\n\nSymmetriska settet\n";
+	Symm.printSet();
+	Symm.printPairs();
+
 	MyRel Trans = Bas.trans_closure();
+	std::cout << "\n\nTransitiva settet\n";
+	Trans.printSet();
+	Trans.printPairs();
+	
 	MyRel Append = Bas;
-		  Append.append(trans);
-	MyRel Comp = Bas.comp(trans);
+		  Append.append(Trans);
+	std::cout << "\n\nAlla par fran MyRel Trans laggs i Append\n";
+	Append.printSet();
+	Append.printPairs();
+	
+	MyRel Comp = Symm.comp(Refl);
+	std::cout << "\n\nSammansattning mellan Symm och Refl\n";
+	Comp.printSet();
+	Comp.printPairs();
+
+	int x;
+	std::cin >> x;
+
 	return 0;
 }
